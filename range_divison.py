1e0faad8fd98f245de4b40444b4bc5998eadf363
vasavioleti <64959255+vasavioleti@users.noreply.github.com>
2020-07-17 21:59:22 +0530

    Update range_divison.py

--- range_divison.py
+++ range_divison.py
@@ -1,6 +1,6 @@
 list_1=[x for x in range(2000,3201) if x % 7 == 0 and x % 5 != 0]
-print(list_1,"foo")
+print("foo")
 list_2=[x for x in range(2000,3201) if x % 7 != 0 and x % 5 == 0]
-print(list_2,"bar")
+print("bar")
 list_3=[x for x in range(2000,3201) if x % 7 == 0 and x % 5 == 0]
-print(list_3,"foobar")
+print("foobar")
